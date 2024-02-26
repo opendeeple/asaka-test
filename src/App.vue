7ba46e0edef6d61b07f37762bd94dcf23960629f
Firdavs Beknazarov <opendeeple@gmail.com>
2024-02-26 15:00:36 +0500

    add collapse to list

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   </header>
   <main>
     <ol v-draggable="[list, {animation: 150}]" class="list">
-      <li class="list-item" v-for="(item, index) in list" v-bind:key="index">
+      <li class="list-item" v-for="(item, index) in list" v-bind:key="index" v-on:click="item.isOpen = !item.isOpen">
         <div class="list-item-heading">
           <!-- number column -->
           <div class="list-item-number">
@@ -56,46 +56,50 @@
         </div>
 
         <!-- subitems -->
-        <ol class="sublist" v-if="item.items" tag="ol">
-          <li class="list-subitem" v-for="(item, subindex) in item.items" v-bind:key="subindex">
-            <div class="sublist-divider" v-if="subindex != 0" />
-            <div class="list-item-row">
-              <!-- subitem number column -->
-              <div class="list-item-number">
-                <div class="list-item-header">
-                  №
+        <template v-if="item.isOpen">
+          <Transition>
+            <ol class="sublist" v-if="item.items" tag="ol">
+              <li class="list-subitem" v-for="(item, subindex) in item.items" v-bind:key="subindex">
+                <div class="sublist-divider" v-if="subindex != 0" />
+                <div class="list-item-row">
+                  <!-- subitem number column -->
+                  <div class="list-item-number">
+                    <div class="list-item-header">
+                      №
+                    </div>
+                    <div class="list-item-body">
+                      {{ index + 1 }}.{{ subindex + 1 }}
+                    </div>
+                  </div>
+                  <!-- subitem name column -->
+                  <div class="list-item-name">
+                    <div class="list-item-header">
+                      Название
+                    </div>
+                    <div class="list-item-body">
+                      <img src="~@/assets/icons/file-icon.svg"/> {{ item.name }}
+                    </div>
+                  </div>
+                  <!-- subitem inorder column -->
+                  <div class="list-item-in-order">
+                    <div class="list-item-header">
+                      Очередность
+                    </div>
+                    <div class="list-item-body">
+                      {{ item.inorder }}
+                    </div>
+                  </div>
+                  <!-- subitem tools -->
+                  <div class="list-item-tools">
+                    <div class="indicator">
+                      <img src="~@/assets/icons/dots.svg" alt="dots">
+                    </div>
+                  </div>
                 </div>
-                <div class="list-item-body">
-                  {{ index + 1 }}.{{ subindex + 1 }}
-                </div>
-              </div>
-              <!-- subitem name column -->
-              <div class="list-item-name">
-                <div class="list-item-header">
-                  Название
-                </div>
-                <div class="list-item-body">
-                  <img src="~@/assets/icons/file-icon.svg"/> {{ item.name }}
-                </div>
-              </div>
-              <!-- subitem inorder column -->
-              <div class="list-item-in-order">
-                <div class="list-item-header">
-                  Очередность
-                </div>
-                <div class="list-item-body">
-                  {{ item.inorder }}
-                </div>
-              </div>
-              <!-- subitem tools -->
-              <div class="list-item-tools">
-                <div class="indicator">
-                  <img src="~@/assets/icons/dots.svg" alt="dots">
-                </div>
-              </div>
-            </div>
-          </li>
-        </ol>
+              </li>
+            </ol>
+          </Transition>
+        </template>
       </li>
     </ol>
   </main>
@@ -105,19 +109,46 @@
 import { ref } from 'vue'
 import DropdownMenu from "@/components/DropdownMenu.vue"
 import { vDraggable } from 'vue-draggable-plus'
-
+const items = [
+  {
+    name: "Максимал фойда (Нац. валюта)",
+    inorder: 1
+  },
+  {
+    name: "On-line (Нац. валюта)",
+    inorder: 2
+  },
+  {
+    name: "Аванс (Нац. валюта)",
+    inorder: 3
+  },
+  {
+    name: "Максимал фойда (Ин. валюта)",
+    inorder: 4
+  },
+  {
+    name: "On-line (Ин. валюта)",
+    inorder: 5
+  },
+  {
+    name: "Аванс (Ин. валюта)",
+    inorder: 6
+  }
+]
 const list = ref([
 { 
   name: "Mastercard", 
   inorder: 1,
   category: [
-    "Mastercard (Standart)",
-    "Mastercard (World)",
-    "Mastercard (Platinum)",
-    "Mastercard (Gold)",
-    "Master card"
+        "Mastercard (Standart)",
+        "Mastercard (World)",
+        "Mastercard (Platinum)",
+        "Mastercard (Gold)",
+        "Master card"
       ],
-      badgeNumber: 12
+      badgeNumber: 12,
+      items: items,
+      isOpen: false
     },
     {
       name: "вклады",
@@ -129,38 +160,16 @@ const list = ref([
         "Максимал фойда"
       ],
       badgeNumber: 9,
-      items: [
-        {
-          name: "Максимал фойда (Нац. валюта)",
-          inorder: 1
-        },
-        {
-          name: "On-line (Нац. валюта)",
-          inorder: 2
-        },
-        {
-          name: "Аванс (Нац. валюта)",
-          inorder: 3
-        },
-        {
-          name: "Максимал фойда (Ин. валюта)",
-          inorder: 4
-        },
-        {
-          name: "On-line (Ин. валюта)",
-          inorder: 5
-        },
-        {
-          name: "Аванс (Ин. валюта)",
-          inorder: 6
-        }
-      ]
+      items: items,
+      isOpen: false
     },
     {
       name: "Кредиты",
       inorder: 3,
       category: [],
-      badgeNumber: 12
+      badgeNumber: 12,
+      items: items,
+      isOpen: false
     }
   ]
 )
